f69d42ece3440ae73a6401a068a31366a69b37ae
James Tsetsekas <jtsetsekas@gmail.com>
2023-12-03 20:28:14 -0500

    add google analytics

--- src/assets/js/google.js
+++ src/assets/js/google.js
@@ -1,14 +1,3 @@
-// Google Tag Manager
-(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-KFNJXD7K');
-
 // Google Analytics tag (gtag.js)
 (function (w, d, s, l, i) {
     w[l] = w[l] || [];
@@ -18,11 +7,11 @@
     j.async = true;
     j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i;
     f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'G-TXGKYHTLDB');
+})(window, document, 'script', 'dataLayer', 'G-SQYX3YVVFL');
 
 window.dataLayer = window.dataLayer || [];
 function gtag() {
     dataLayer.push(arguments);
 }
 gtag('js', new Date());
-gtag('config', 'G-TXGKYHTLDB');
+gtag('config', 'G-SQYX3YVVFL');
